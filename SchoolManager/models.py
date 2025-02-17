b996146f18a6a1b94de00f48855be808737277d2
Cotton-candy-is-not-available <156360240+Cotton-candy-is-not-available@users.noreply.github.com>
2025-02-17 15:29:59 -0500

    bug fix

--- SchoolManager/models.py
+++ SchoolManager/models.py
@@ -5,7 +5,9 @@ class Event(models.Model):
     event_name = models.CharField(max_length=100, default= "[Event Name]")#event name
     date_of_event = models.DateField(null = True, blank = True)#add start and end date
     description = models.TextField()
-    # top_imortance = intergerfield(false)
+    most_important = models.BooleanField(default=False)
+    mid_important = models.BooleanField(default=False)
+    least_important = models.BooleanField(default=False)
     #each event can be associated to only one calendar
 
     def __str__(self):
