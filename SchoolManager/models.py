076eb3f3bf9f378854647a3e41c6b783216bcccc
Cotton-candy-is-not-available <156360240+Cotton-candy-is-not-available@users.noreply.github.com>
2025-03-07 18:31:20 -0500

    fixed timezone, now all events display on correct date and made it so calendar only displays name of the event

--- SchoolManager/models.py
+++ SchoolManager/models.py
@@ -2,6 +2,26 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+#------ Calendar --------------
+class Calendar(models.Model): #each calendar can have many events associated to it
+    date = models.DateField()
+    day_number = models.IntegerField()
+    text_box = models.TextField(null=True, blank=True)
+
+#------ Events --------------
+class Event(models.Model):
+    # Start_time = models.DateTimeField(null=True, blank=True)
+    # End_time = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, editable = False)
+    event_name = models.CharField(max_length=100, default= "[Event Name]")#event name
+    description = models.TextField()
+    date_of_event = models.DateField(null = True, blank = True)
+
+    def __str__(self):
+        return f"{self.event_name},  on the {self.date_of_event}"
+
+
+#------ To do list --------------
 class TD_list(models.Model):
     List_name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, editable = False)
@@ -21,21 +41,6 @@ class Task(models.Model):
         return self.list.List_name + ": " + self.description
 
 
-class Event(models.Model):
-    # Start_time = models.DateTimeField(null=True, blank=True)
-    # End_time = models.DateTimeField(null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, editable = False)
-    event_name = models.CharField(max_length=100, default= "[Event Name]")#event name
-    description = models.TextField()
-    date_of_event = models.DateField(null = True, blank = True)
-
-    def __str__(self):
-        return f"{self.event_name},  on the {self.date_of_event}"
-
-class Calendar(models.Model): #each calendar can have many events associated to it
-    date = models.DateField()
-    day_number = models.IntegerField()
-    text_box = models.TextField(null=True, blank=True)
 #
 # class Event(models.Model):
 #     date_of_event = models.DateField(null = True, blank = True)
