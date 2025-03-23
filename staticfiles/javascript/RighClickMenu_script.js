faee43eebe79b3d3755b6d5d6d274fb53f227b5a
Cotton-candy-is-not-available <156360240+Cotton-candy-is-not-available@users.noreply.github.com>
2025-03-23 12:08:08 -0400

    prevented context menus from appearing outside of the  event. They now only appear when you rigth click an event

--- staticfiles/javascript/RighClickMenu_script.js
+++ staticfiles/javascript/RighClickMenu_script.js
@@ -1,17 +1,35 @@
-const day_1Event_Display = document.querySelector(".day1EventDisplay");
+const day_1Event_Display = document.querySelectorAll(".eventBoxes");
 const Event_Right_Click_Menu = document.querySelector(".EventRightClickMenu")
 
-day_1Event_Display.addEventListener("contextmenu", (event) => {
-   event.preventDefault();
-    Event_Right_Click_Menu.classList.add("active")
-//Position of the menu
-    let topPosition = event.clientY;
-    let leftPosition = event.clientX;
+for (let i = 0; i < day_1Event_Display.length; i++) {
+    //Adds event listener to each  Event being displayed
 
-    Event_Right_Click_Menu.style.top = topPosition + "px";
-    Event_Right_Click_Menu.style.left = leftPosition + "px";
-});
 
+    day_1Event_Display[i].addEventListener("contextmenu", (event) => {
+        console.log(" Start i: ", i)
+
+        event.preventDefault();//prevents regular right click menu from appearing
+        event.stopPropagation(); //important!!
+
+        console.log("before active i: ", i)
+        //
+        // Event_Right_Click_Menu.classList.item(i)
+        // console.log("test i: ", Event_Right_Click_Menu.classList.item(i))
+
+
+        Event_Right_Click_Menu.classList.add("active")
+        console.log("after active i: ", i)
+        //Position of the menu
+        let topPosition = event.clientY;
+        let leftPosition = event.clientX;
+
+        Event_Right_Click_Menu.style.top = topPosition + "px";
+        Event_Right_Click_Menu.style.left = leftPosition + "px";
+    });
+}
+
+
+//Closes window when you click somewhere else
 window.addEventListener("click", () => {
     Event_Right_Click_Menu.classList.remove("active");
 })
