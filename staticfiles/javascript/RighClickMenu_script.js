4a5504f97e34d49f164abf2780cff234d80b0190
Cotton-candy-is-not-available <156360240+Cotton-candy-is-not-available@users.noreply.github.com>
2025-03-23 16:39:03 -0400

    right click button works with all events

--- staticfiles/javascript/RighClickMenu_script.js
+++ staticfiles/javascript/RighClickMenu_script.js
@@ -1,31 +1,46 @@
-const day_1Event_Display = document.querySelectorAll(".eventBoxes");
-const Event_Right_Click_Menu = document.querySelector(".EventRightClickMenu")
+let day_1Event_Display = document.querySelectorAll(".eventBoxes");
+let Event_Right_Click_Menu = document.querySelectorAll(".EventRightClickMenu")
+
+function Menu(i, topPosition, leftPosition){
+            console.log(" 1 event: ", Event_Right_Click_Menu[i])
+        //Turns on context menu
+        Event_Right_Click_Menu[i].classList.add("active")
+
+    //Position
+        Event_Right_Click_Menu[i].style.top = topPosition + "px";
+        Event_Right_Click_Menu[i].style.left = leftPosition + "px";
+
+//Closes window when you click somewhere else
+        window.addEventListener("click", () => {
+    Event_Right_Click_Menu[i].classList.remove("active");
+})
+
+}
+
 
 for (let i = 0; i < day_1Event_Display.length; i++) {
+
     //Adds event listener to each  Event being displayed
-
-
     day_1Event_Display[i].addEventListener("contextmenu", (event) => {
-        console.log(" Start i: ", i)
 
         event.preventDefault();//prevents regular right click menu from appearing
         event.stopPropagation(); //important!!
 
         console.log("before active i: ", i)
 
-        Event_Right_Click_Menu.classList.add("active")
         console.log("after active i: ", i)
         //Position of the menu
         let topPosition = event.clientY;
         let leftPosition = event.clientX;
 
-        Event_Right_Click_Menu.style.top = topPosition + "px";
-        Event_Right_Click_Menu.style.left = leftPosition + "px";
+
+        //Displays and closes context menu and positions it
+        Menu(i, topPosition, leftPosition);
+
     });
+
+
+
+
 }
 
-
-//Closes window when you click somewhere else
-window.addEventListener("click", () => {
-    Event_Right_Click_Menu.classList.remove("active");
-})
