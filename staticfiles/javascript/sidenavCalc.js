15fdceed44c9a2f7ee7bf96053e3e2e66663f15b
MariyaNeyrAl <mariya.neyral@gmail.com>
2025-03-26 09:32:28 -0400

    AHHHHH FIXING

--- staticfiles/javascript/sidenavCalc.js
+++ staticfiles/javascript/sidenavCalc.js
@@ -12,3 +12,18 @@ function adjustHeight() {
     sideNav.style.height = `${Math.max(contentHeight, window.innerHeight)}px`;
   }
 }
+
+/* OLLLD ATTEMPT
+window.addEventListener('load', adjustHeight);
+window.addEventListener('resize', adjustHeight);
+
+
+function adjustHeight() {
+  const sideNav = document.querySelector('.sideNav');
+  const mainContent = document.querySelector('.main_content');
+
+  const contentHeight = mainContent.scrollHeight;
+
+  sideNav.style.height = `${Math.max(contentHeight, window.innerHeight)}px`;
+}
+ */
